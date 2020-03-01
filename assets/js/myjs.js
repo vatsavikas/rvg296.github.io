9b9187f41ad8aaf60737d83935b41e5298fb64dc
Rohit Mendadhala <rvg296@gmail.com>
2020-03-01 00:14:02 -0600

    Initial website design including content

---
+++ assets/js/myjs.js
@@ -0,0 +1,9 @@
+function tfsFunction() {
+//    var x = document.querySelector('p');
+   var x = document.querySelector('#tfsId');
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
